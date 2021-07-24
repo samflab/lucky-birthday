6942d8519f391fd13e9a630dbf4337069fe53414
Masudha Meher <kasturimeh@gmail.com>
2021-07-24 17:54:46 +0530

    fixed: split called on undefined

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var birthday = document.getElementById("date");
 var luckyNumber = document.getElementById("lucky-number");
 const btn = document.getElementById("btn");
-var result = document.querySelector(".result");
+var result = document.querySelector("#result");
+var errorMesg = document.querySelector("#error");
 const crossBtn = document.querySelector(".cross-btn");
 const privacyMessage = document.querySelector(".attention-msg");
 
@@ -12,16 +13,22 @@ crossBtn.addEventListener("click", (e) => {
 
 function check(e) {
   e.preventDefault();
-  birthday = birthday.value.split("-").join(""); //removing ' - '
-  birthday = Number(birthday);
-  console.log(birthday); //converting date to number
-  luckyNumber = Number(luckyNumber.value);
-
-  result.innerHTML = " ";
-
-  if (birthday % luckyNumber === 0) {
+  const birthdayArray = birthday.value.split("-"); //removing ' - '
+  let sum = 0;
+  birthdayArray.map((string) => {
+    //to access every number in string and add into sum
+    for (let i = 0; i < string.length; i++) {
+      sum = sum + string[i];
+    }
+  });
+  sum = Number(sum);  //converting to number type
+  
+  if (sum % Number(luckyNumber.value) === 0) {
     result.innerHTML = "Yay! Its your lucky birthday.";
-  } else {
-    result.innerHTML = "I'll wish you luck in your next birthday";
+    errorMesg.innerHTML =" ";
+  } 
+  if(sum % Number(luckyNumber.value) !== 0) {
+    error.innerHTML = "I'll wish you luck in your next birthday";
+    result.innerHTML = " ";
   }
 }
